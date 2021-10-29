c89f4d208b3f07d3b13420a1f16c58e3792d4a87
Kaoru NAKAMURA <kaorun55@hololab.co.jp>
2021-10-29 16:52:43 +0900

    アイコンを削除

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/HoloLab_logo-02.png`, // This path is relative to the root of the site.
+        //icon: `src/images/HoloLab_logo-02.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
